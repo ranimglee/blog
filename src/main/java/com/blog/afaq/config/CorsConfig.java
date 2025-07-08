2efb0433fc194fb2b58376537e2bbd2ec0a441a6
ranimglee <ranim.abassi@esprit.tn>
2025-07-08 21:54:55 +0100

    fix(cors): allow netlify frontend

--- src/main/java/com/blog/afaq/config/CorsConfig.java
+++ src/main/java/com/blog/afaq/config/CorsConfig.java
@@ -2,28 +2,46 @@ package com.blog.afaq.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.Arrays;
 
 @Configuration
 public class CorsConfig {
 
+
     @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedOrigins(
-                                "https://efaq.netlify.app",
-                                "https://efaqadmin.netlify.app",
-                                "https://blog-production-5144.up.railway.app"
-                        )
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                        .allowedHeaders("*")
-                        .exposedHeaders("Content-Disposition")
-                        .allowCredentials(true);
-            }
-        };
+    public CorsFilter corsFilter()
+    {
+        final UrlBasedCorsConfigurationSource source=new UrlBasedCorsConfigurationSource();
+        final CorsConfiguration config=new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.setAllowedOrigins(Arrays.asList(
+                "https://efaq.netlify.app",
+                "https://efaqadmin.netlify.app",
+                "https://blog-production-5144.up.railway.app"
+
+
+        ));
+        config.setAllowedHeaders(Arrays.asList(
+                HttpHeaders.ORIGIN,
+                HttpHeaders.CONTENT_TYPE,
+                HttpHeaders.ACCEPT,
+                HttpHeaders.AUTHORIZATION
+        ));
+        config.setAllowedMethods(Arrays.asList(
+                "GET",
+                "POST",
+                "PATCH",
+                "DELETE",
+                "PUT"
+        ));
+        source.registerCorsConfiguration("/**",config);
+        return new CorsFilter(source);
     }
 }
