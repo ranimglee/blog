4296425b516d1988153988fb8697169805efe85b
ranimglee <ranim.abassi@esprit.tn>
2025-11-05 19:33:47 +0100

    cors issues

--- src/main/java/com/blog/afaq/config/SecurityConfig.java
+++ src/main/java/com/blog/afaq/config/SecurityConfig.java
@@ -3,11 +3,9 @@ package com.blog.afaq.config;
 import com.blog.afaq.security.JwtAuthenticationFilter;
 import com.blog.afaq.security.JwtTokenProvider;
 import com.blog.afaq.service.CustomUserDetailsService;
-import lombok.AllArgsConstructor;
-import lombok.RequiredArgsConstructor;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
@@ -16,7 +14,6 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
